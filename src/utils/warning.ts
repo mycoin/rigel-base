f51dba08aff98fe1a0ac69ac69c49549cb001b8c
mycoin <1845440+mycoin@users.noreply.github.com>
2023-07-12 11:48:24 +0800

    update

--- src/utils/warning.ts
+++ src/utils/warning.ts
@@ -1,7 +1,10 @@
-/* eslint-disable no-console */
 import universalEnv from '../universalEnv'
 
-// 打印警告信息
+/**
+ * 打印警告信息
+ *
+ * @param messages
+ */
 const warning = (...messages: any[]): void => {
   if (universalEnv.isDevelopment || universalEnv.isDebugger) {
     console.warn(...messages)
